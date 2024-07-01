2d81f88e6d272a96ba201db8866c98fb8b6a7e72
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-01 09:41:38 -0600

    Utilizing self.get_driver now

--- bannerdriver/driver.py
+++ bannerdriver/driver.py
@@ -24,15 +24,18 @@ class BannerDriver:
         Get the Selenium WebDriver object
         :return: webdriver object
         """
+        if not self.driver:
+            self.driver = webdriver.Chrome(
+                options=self.options)
         return self.driver
 
     def _nav_to_login(self):
-        self.driver = webdriver.Chrome(options=self.options)
-        self.driver.get(f"https://{self.env}banner.montana.edu/"
+        driver = self.get_driver()
+        driver.get(f"https://{self.env}banner.montana.edu/"
                         "applicationNavigator/seamless")
-        WebDriverWait(self.driver, self.timeout).until(
+        WebDriverWait(driver, self.timeout).until(
             EC.visibility_of_element_located((By.ID, "username")))
-        WebDriverWait(self.driver, self.timeout).until(
+        WebDriverWait(driver, self.timeout).until(
             EC.visibility_of_element_located((By.ID, "password")))
 
     def _enter_credentials(self):
@@ -40,6 +43,7 @@ class BannerDriver:
         Enter the username and password into the login form
         :return: None
         """
+        driver = self.get_driver()
 
         def _identify_error() -> str:
             """
@@ -47,7 +51,7 @@ class BannerDriver:
             :return: text of the error message
             """
             try:
-                login_form = WebDriverWait(self.driver, 1).until(
+                login_form = WebDriverWait(driver, 1).until(
                     EC.visibility_of_element_located((By.CLASS_NAME, "login-form")))
                 error_div = login_form.find_element(By.CLASS_NAME, "alert-danger")
                 if error_div:
@@ -59,11 +63,11 @@ class BannerDriver:
             return ""
 
         # Enter the username and password
-        input_username = WebDriverWait(self.driver, self.timeout).until(
+        input_username = WebDriverWait(driver, self.timeout).until(
             EC.visibility_of_element_located((By.ID, "username")))
-        input_password = WebDriverWait(self.driver, self.timeout).until(
+        input_password = WebDriverWait(driver, self.timeout).until(
             EC.visibility_of_element_located((By.ID, "password")))
-        button_submit = WebDriverWait(self.driver, self.timeout).until(
+        button_submit = WebDriverWait(driver, self.timeout).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@name='_eventId_proceed']")))
         input_username.send_keys(self.username)
         input_password.send_keys(self.password)
@@ -71,8 +75,8 @@ class BannerDriver:
 
         if e := _identify_error():
             print(f"Error: {e}")
-            self.driver.quit()
-        WebDriverWait(self.driver, self.timeout).until(
+            driver.quit()
+        WebDriverWait(driver, self.timeout).until(
             EC.title_is("Two-Factor Authentication"))
 
     def _handle_2fa(self, method="push"):
@@ -81,12 +85,13 @@ class BannerDriver:
         :param method: "push", "sms", or "call"
         :return: None
         """
-        WebDriverWait(self.driver, self.timeout).until(
+        driver = self.get_driver()
+        WebDriverWait(driver, self.timeout).until(
             EC.title_is("Two-Factor Authentication"))
         if method == "push":
             print("Please accept the push notification on your phone")
             xpath = "//button[text()='Send Me a Push ']"
-            button = WebDriverWait(self.driver, 300).until(
+            button = WebDriverWait(driver, 300).until(
                 EC.element_to_be_clickable((By.XPATH, xpath)))
             button.click()
         elif method == "sms":
@@ -99,7 +104,7 @@ class BannerDriver:
             raise ValueError("Invalid 2FA method")
 
         # Wait for the user to complete the 2FA process
-        WebDriverWait(self.driver, self.timeout).until(
+        WebDriverWait(driver, self.timeout).until(
             EC.title_is("Application Navigator"))
         print("Successfully logged in")
 
