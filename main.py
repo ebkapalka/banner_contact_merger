41e3c587d473f0bf510056ce9ed8ef3fdac2f4b7
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-01 16:55:31 -0600

    GoodDriver and BadDriver are now separate classes, so the logic to create both has been split into two separate functions.  They also both take a GID number now

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
+from bannerdriver.drivers.driver_good import GoodDriver
+from bannerdriver.drivers.driver_bad import BadDriver
 from command_queue.sqlite import SQLiteManager
-from bannerdriver.driver import BannerDriver
 
-import threading
+import multiprocessing
 import os
 
 
-def create_and_run_driver(name: str, db_uri: str):
+def start_good_worker(username: str, password: str, db_uri: str, gid: str, env: str) -> None:
     """
-    Create and run a BannerDriver instance
-    :param name: Name of the worker
-    :param db_uri: URI of the SQLite database
+    Start the worker to process the good gid record
+    :param username: username for Banner login
+    :param password: password for Banner login
+    :param db_uri: uri for the command queue database
+    :param gid: GID of the good student record
+    :param env: 'prod' or 'test', depending on the environment
     :return: None
     """
-    queue = SQLiteManager(db_uri, name)
-    banner_username = os.environ.get("BANNER_USERNAME")
-    banner_password = os.environ.get("BANNER_PASSWORD")
-    BannerDriver(banner_username, banner_password, name, queue)
+    queue = SQLiteManager(db_uri, worker_name="good")
+    worker = GoodDriver(username, password, queue, gid, env)
+    worker.main_loop()
+
+
+def start_bad_worker(username: str, password: str, db_uri: str, gid: str, env: str) -> None:
+    """
+    Start the worker to process the bad gid record
+    :param username: username for Banner login
+    :param password: password for Banner login
+    :param db_uri: uri for the command queue database
+    :param gid: GID of the bad student record
+    :param env: 'prod' or 'test', depending on the environment
+    :return: None
+    """
+    queue = SQLiteManager(db_uri, worker_name="bad")
+    worker = BadDriver(username, password, queue, gid, env)
+    worker.main_loop()
 
 
 if __name__ == '__main__':
-    queue_db = "sqlite:///command_queue/db.sqlite"
-    name_good = "worker_good"
-    name_bad = "worker_bad"
+    good_gid = '-02179809'
+    bad_gid = '-02179899'
+    queue_db_uri = "sqlite:///command_queue/db.sqlite"
+    environment = os.environ.get("BANNER_ENV", 'prod')
+    banner_username = os.environ.get("BANNER_USERNAME")
+    banner_password = os.environ.get("BANNER_PASSWORD")
 
-    thread_good = threading.Thread(target=create_and_run_driver, args=(name_good, queue_db))
-    thread_bad = threading.Thread(target=create_and_run_driver, args=(name_bad, queue_db))
-    thread_good.start()
-    thread_bad.start()
-    thread_good.join()
-    thread_bad.join()
+    good_process = multiprocessing.Process(target=start_good_worker,
+                                           args=(banner_username,
+                                                 banner_password,
+                                                 queue_db_uri,
+                                                 good_gid,
+                                                 environment))
+    bad_process = multiprocessing.Process(target=start_bad_worker,
+                                          args=(banner_username,
+                                                banner_password,
+                                                queue_db_uri,
+                                                bad_gid,
+                                                environment))
+    good_process.start()
+    bad_process.start()
+    good_process.join()
+    bad_process.join()
