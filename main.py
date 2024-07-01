3c791195e75e413470bd6165c3fb1214ed4fc49c
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-01 12:19:00 -0600

    Added threading so both workers could operate concurrently

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from command_queue.sqlite import SQLiteManager
 from bannerdriver.driver import BannerDriver
 
+import threading
 import os
 
 
-if __name__ == '__main__':
+def create_and_run_driver(name: str, db_uri: str):
+    """
+    Create and run a BannerDriver instance
+    :param name: Name of the worker
+    :param db_uri: URI of the SQLite database
+    :return: None
+    """
+    queue = SQLiteManager(db_uri, name)
     banner_username = os.environ.get("BANNER_USERNAME")
     banner_password = os.environ.get("BANNER_PASSWORD")
+    BannerDriver(banner_username, banner_password, name, queue)
 
+
+if __name__ == '__main__':
     queue_db = "sqlite:///command_queue/db.sqlite"
-    queue_good = SQLiteManager(queue_db, "worker_good")
-    queue_bad = SQLiteManager(queue_db, "worker_bad")
-    driver_good = BannerDriver(banner_username, banner_password, queue_good)
-    driver_bad = BannerDriver(banner_username, banner_password, queue_bad)
+    name_good = "worker_good"
+    name_bad = "worker_bad"
+
+    thread_good = threading.Thread(target=create_and_run_driver, args=(name_good, queue_db))
+    thread_bad = threading.Thread(target=create_and_run_driver, args=(name_bad, queue_db))
+    thread_good.start()
+    thread_bad.start()
+    thread_good.join()
+    thread_bad.join()
