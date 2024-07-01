9be11b7052b7ffd980955ac5448110b20c9db133
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-01 16:58:24 -0600

    Added command retrieval to the main loop

--- bannerdriver/drivers/driver_good.py
+++ bannerdriver/drivers/driver_good.py
@@ -1,7 +1,12 @@
 from bannerdriver.drivers.driver_base import BannerDriver
 
+import time
+
 
 class GoodDriver(BannerDriver):
+    """
+    Driver to process the good record
+    """
     def main_loop(self) -> None:
         """
         Loop used for adding data to the good record
@@ -9,5 +14,5 @@ class GoodDriver(BannerDriver):
         """
         driver = self.get_driver()
         while True:
-            # Do something
-            pass
+            # TODO: change database structure so this works
+            command = self.queue.get_next_command(self.gid)
