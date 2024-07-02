96ba93b35cbc7151a3c60fa94e22524749cedb53
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-02 17:08:56 -0600

    Added some test function calls to verify functions in functions_general

--- bannerdriver/drivers/driver_good.py
+++ bannerdriver/drivers/driver_good.py
@@ -1,3 +1,5 @@
+from bannerdriver.functions_general import extract_input_values, switch_to_tab
+from bannerdriver.navigation import nav_to_form, enter_gid
 from bannerdriver.drivers.driver_base import BannerDriver
 
 import time
@@ -13,6 +15,13 @@ class GoodDriver(BannerDriver):
         :return: None
         """
         driver = self.get_driver()
-        while True:
-            # TODO: change database structure so this works
-            command = self.queue.get_next_command(self.gid)
+        nav_to_form(self, "SPAIDEN")
+        enter_gid(self, self.gid)
+        switch_to_tab(self, "Address")
+        v = extract_input_values(self)
+        print(v)
+        time.sleep(300)
+
+    # while True:
+        #     # TODO: change database structure so this works
+        #     command = self.queue.get_next_command(self.gid)
