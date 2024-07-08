8647126019219554ecbae69da5136b9491905545
Eric Kapalka <eric.kapalka@montana.edu>
2024-07-08 11:03:15 -0600

    Moved script paths dict from here to a config file for ease of maintenance. Added initialize_scripts and load_scripts_from_yaml functions

--- bannerdriver/functions_js.py
+++ bannerdriver/functions_js.py
@@ -1,52 +1,63 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+import yaml
 import os
 
 from bannerdriver.functions_general import switch_iframe, get_driver
+from bannerdriver.drivers.driver_base import BannerDriver
 
-SCRIPT_NAMES = {
-    "get_emails": "bannerdriver/js_scripts/get_emails.js",
-    "add_emails": "bannerdriver/js_scripts/add_emails.js",
-    "delete_emails": "bannerdriver/js_scripts/delete_emails.js",
-    "get_phone_numbers": "bannerdriver/js_scripts/get_phone_numbers.js",
-    "add_phone_numbers": "bannerdriver/js_scripts/add_phone_numbers.js",
-    "delete_phone_numbers": "bannerdriver/js_scripts/delete_phone_numbers.js",
-    "get_addresses": "bannerdriver/js_scripts/get_addresses.js",
-    "add_addresses": "bannerdriver/js_scripts/add_addresses.js",
-    "delete_addresses": "bannerdriver/js_scripts/delete_addresses.js",
-    "get_test_scores": "bannerdriver/js_scripts/get_test_scores.js",
-    "add_test_scores": "bannerdriver/js_scripts/add_test_scores.js",
-    "delete_test_scores": "bannerdriver/js_scripts/delete_test_scores.js",
-    "delete_alt_ids": "bannerdriver/js_scripts/delete_alt_ids.js",
-    # Add more scripts as needed
-}
 script_cache = {}  # Cache for the content of JavaScript files
 
 
-def load_js_scripts(script_names):
+def initialize_scripts(yaml_file: str) -> None:
+    """
+    Load and cache JavaScript files into memory from a YAML file.
+    :param yaml_file: Path to the YAML file containing script names and paths.
+    :return: None
+    """
+    script_names = _load_script_names_from_yaml(yaml_file)
+    _load_js_scripts(script_names)
+
+
+def _load_script_names_from_yaml(yaml_file: str) -> dict[str, str]:
+    """
+    Load script names and paths from a YAML file.
+    :param yaml_file: Path to the YAML file.
+    :return: Dictionary of script names and file paths.
+    """
+    with open(yaml_file, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def _load_js_scripts(script_names: dict[str, str]) -> None:
     """
     Load and cache JavaScript files into memory.
     :param script_names: Dictionary of script names and file paths.
+    :return: None
     """
+    global script_cache
     for name, path in script_names.items():
         if not os.path.isfile(path):
-            raise FileNotFoundError(f"The script file '{path}' does not exist.")
+            print(f"The script file '{path}' does not exist.")
+            continue
         try:
             with open(path, 'r') as file:
                 script_cache[name] = file.read()
         except Exception as e:
-            print(type(e))
-            raise Exception(f"Error reading the script file '{path}': {e}")
+            print(f"The script file '{path}' could not be read.")
+            continue
 
 
-def execute_js(driver, script_name, *args):
+def execute_js(driver: WebDriver | BannerDriver, script_name: str, *args) -> any:
     """
     Executes a cached JavaScript file on the current page using Selenium WebDriver.
-    :param driver: webdriver object
+    :param driver: WebDriver object.
     :param script_name: Name of the cached JavaScript script to execute.
     :param args: Arguments to pass to the JavaScript function.
-    :raises ValueError: If the script name is invalid.
+    :raises ValueError: If the script name is invalid or scripts are not loaded.
     :return: Result of the executed script or None.
     """
+    if not script_cache:
+        raise ValueError("No JavaScript scripts have been loaded.")
     driver = get_driver(driver)
     switch_iframe(driver)
     js_script = script_cache.get(script_name)
@@ -59,9 +70,4 @@ def execute_js(driver, script_name, *args):
         result = driver.execute_script(js_code, *args)
         return result
     except Exception as e:
-        print(type(e))
         raise Exception(f"Error executing the script '{script_name}': {e}")
-
-
-# Load all JavaScript scripts into the cache
-load_js_scripts(SCRIPT_NAMES)
